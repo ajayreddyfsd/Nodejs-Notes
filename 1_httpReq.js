bc320e66c3361046401c0673f37cb4cd835981d7
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-03-08 08:46:48 -0500

    .

--- 1_httpReq.js
+++ 1_httpReq.js
@@ -1,12 +1,18 @@
-const http = require('http');
+const http = require("http");
 
-const req = http.request('http://www.google.com', (res) => {
-    res.on('data', (chunk) => {
-        console.log('chunk = ', chunk)
-    })
-    res.on('end', () => {
-        console.log('done!!!!!!!!!!!')
-    })
-})
+//3 callbacks
+//one to make a request and once u get response, the callback is placed onto CBQ and then onto CS
+//then again 2 callbacks for data and end events
+//whenever a chunk is received, its callback is placed onto CBQ and then onto CS, happens so many times
+//when end of data is detected, places its callback onto CBQ and then onto CS
+const req = http.request("http://www.google.com", (res) => {
+  res.on("data", (chunk) => {
+    console.log("chunk = ", chunk);
+  });
+  res.on("end", () => {
+    console.log("done!!!!!!!!!!!");
+  });
+});
 
-req.end()
+//to initiate the request
+req.end();
--- 7_simpleWebServer.js
+++ 7_simpleWebServer.js
@@ -1,23 +1,25 @@
 //we need this http module to write a server
-const http = require('http')
-
+const http = require("http");
 
 //this is a simple server code, which takes a callback with two args, res and req
 const server = http.createServer((req, res) => {
-
   //writing simple header for the response
-  res.writeHead(200, { "content-type": "text/plain" })
-  
+  res.writeHead(200, { "content-type": "text/plain" });
+
   //writing a simple response to show to the user
-  res.end('Welcome to Nodejs!!!!!!!!')
+  res.end("Welcome to Nodejs!!!!!!!!");
+});
 
-})
-
-
-//now that server is done! we need to activate it and make sure it is active all the time, 
+//now that server is done! we need to activate it and make sure it is active all the time,
 // so we start the listen method on any specific port
 server.listen(2898, () => {
-
   //this is a simple message to be printed to console when server is activated so user knows the link to find the response
-  console.log('check local host 2898')
-})
+  console.log("check local host 2898");
+});
+
+//flow of execution
+//first, server is setup on that port and starts listening
+//once it is ready, its callback is placed onto CBQ and then onto CS and is exed
+//now since the server is actively listening, if it detects any request,
+// it triggers the createServer() callback, placing it onto CBQ and then onto CS
+//now js drafts a response and sends it
