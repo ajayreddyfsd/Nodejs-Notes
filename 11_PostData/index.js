170cb521c8af0d6e5205b1f5a9c9cdfde968ec40
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-03-09 07:01:25 -0400

    submitting and storing form data using createServer

---
+++ 11_PostData/index.js
@@ -0,0 +1,26 @@
+const http = require("http");
+
+//we need this to extract form data
+const querystring = require("querystring");
+
+const server = http.createServer((req, res) => {
+  if (req.method === "POST" && req.url === "/submit-data") {
+    let dataReceived = "";
+    //need to listen to data - streams and collect the data
+    req.on("data", (chunk) => {
+      dataReceived = dataReceived + chunk;
+    });
+
+    //finally we need to listen to end event listener also
+    req.on("end", () => {
+      //we need to parse the received data
+      const parsedData = querystring.parse(dataReceived);
+
+      console.log(parsedData);
+    });
+  }
+});
+
+server.listen(2898, () => {
+  console.log("check on port 2898");
+});
